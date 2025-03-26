306d9cb6ee15e6df53caccfb7827c142ee69b734
mjilani98 <mjilani@emich.edu>
2025-03-26 13:06:23 -0400

    Created an app interface

--- app/src/main/java/com/example/hw4q1/AppInterface.java
+++ app/src/main/java/com/example/hw4q1/AppInterface.java
@@ -17,21 +17,12 @@ public class AppInterface extends RelativeLayout
     //the size of the board
     private final int BOARD_SIZE = 3;
 
-    //goal board text view
-    private TextView[][] goalBoard;
-    //grid for the goal board
-    private GridLayout grid;
-
     //initial board text view
     private TextView[][] initialBoard;
     //grid for the initial board
     private GridLayout initialGrid;
 
-    //Buttons
-    private Button upButton;
-    private Button downButton;
-    private Button rightButton;
-    private Button leftButton;
+
 
 
 
@@ -75,11 +66,11 @@ public class AppInterface extends RelativeLayout
                 initialBoard[x][y].setText(" "); //Here
                 initialBoard[x][y].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                 initialBoard[x][y].setGravity(Gravity.CENTER);
-                initialBoard[x][y].setTextSize(TypedValue.COMPLEX_UNIT_SP,25 );
+                initialBoard[x][y].setTextSize(TypedValue.COMPLEX_UNIT_SP,35 );
                 initialBoard[x][y].setTextColor(Color.parseColor("#FF000000"));
                 GridLayout.LayoutParams params = new GridLayout.LayoutParams( );
-                params.width = 80*DP;
-                params.height  = 80*DP;
+                params.width = 150*DP;
+                params.height  = 150*DP;
                 params.rowSpec = GridLayout.spec(x, 1);
                 params.columnSpec = GridLayout.spec(y, 1);
                 params.topMargin = params.bottomMargin = 1;
@@ -96,60 +87,6 @@ public class AppInterface extends RelativeLayout
 
         addView(initialGrid);
         //end initial board
-
-
-
-
-        //create goal board
-        //create a grid layout inside relative layout
-        grid = new GridLayout(context);
-        grid.setId(GridLayout.generateViewId());
-        grid.setRowCount(BOARD_SIZE);
-        grid.setColumnCount(BOARD_SIZE);
-
-        //setting layouts for the grid
-        LayoutParams gridParams = new LayoutParams
-                (
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
-                );
-        gridParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        gridParams.addRule(RelativeLayout.BELOW,initialGrid.getId());
-
-        //setting the grid layouts to the grid
-        grid.setLayoutParams(gridParams);
-
-        //creating a goal board
-        goalBoard = new TextView[BOARD_SIZE][BOARD_SIZE];
-        int goalBoardNums = 1; //numbers that will be printed on the board
-        for(int x = 0 ;  x < BOARD_SIZE ; x++)
-        {
-            for(int y =0  ; y < BOARD_SIZE ; y++)
-            {
-                goalBoard[x][y] = new TextView(context);
-                goalBoard[x][y].setBackgroundColor(Color.parseColor("#00A86B"));
-                goalBoard[x][y].setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                goalBoard[x][y].setGravity(Gravity.CENTER);
-                goalBoard[x][y].setTextSize(TypedValue.COMPLEX_UNIT_SP,25 );
-                goalBoard[x][y].setTextColor(Color.parseColor("#FF000000"));
-                GridLayout.LayoutParams params = new GridLayout.LayoutParams( );
-                params.width = 80*DP;
-                params.height  = 80*DP;
-                params.rowSpec = GridLayout.spec(x, 1);
-                params.columnSpec = GridLayout.spec(y, 1);
-                params.topMargin = params.bottomMargin = 1;
-                params.leftMargin = params.rightMargin = 1;
-
-                if (x == 0) params.topMargin = 200;
-                goalBoard[x][y].setLayoutParams(params);
-                grid.addView(goalBoard[x][y]);
-
-                goalBoardNums += 1;
-            }
-        }
-
-        addView(grid);
-
 
         //setting the background screen color
         setBackgroundColor(Color.parseColor("#DDDDDD"));
@@ -170,45 +107,5 @@ public class AppInterface extends RelativeLayout
         }
     }
 
-
-    //method to display the goal board to the screen
-    public void drawGoalBoard(char[][] goal)
-    {
-        for(int x = 0 ;  x < BOARD_SIZE ; x++)
-        {
-            for (int y = 0; y < BOARD_SIZE; y++)
-            {
-                goalBoard[x][y].setText(""+goal[x][y]);
-            }
-        }
-    }
-
-    //method to return the button id
-    public int findButton(Button button)
-    {
-        int id= 0 ;
-        //if up button is clicked, return 1
-        if(button.getId() == upButton.getId())
-        {
-            id = 1;
-        }
-        //if down button is clicked, return 2
-        if(button.getId() == downButton.getId())
-        {
-            id = 2;
-        }
-        //if left button is clicked, return 3
-        if(button.getId() == leftButton.getId())
-        {
-            id = 3;
-        }
-        //if right button is clicked, return 4
-        if(button.getId() == rightButton.getId())
-        {
-            id = 4;
-        }
-
-        return id;
-    }
 
 }
--- app/src/main/java/com/example/hw4q1/MainActivity.java
+++ app/src/main/java/com/example/hw4q1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hw4q1;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,68 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    //create an app interface
+    //Initialize an app interface
     private AppInterface appInterface;
+
+    //Initialize a game
+    private Game game;
+
+    //Initialize gesture detector
+    private GestureDetector gestureDetector;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-        //display the screen
-        appInterface = new AppInterface();
+        //create a new game
+        game = new Game();
 
+        //create app interface
+        appInterface = new AppInterface(this );
+
+        //display app interface
+        setContentView(appInterface);
+
+        //draw board on the screen
+        appInterface.drawCurrentBoard(game.getCurrentBoard());
+
+        //create a swipe detector
+        SwipeDetecotr detector = new SwipeDetecotr();
+
+        //create a gesture detector
+        gestureDetector = new GestureDetector(this, detector);
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        gestureDetector.onTouchEvent(event);
+
+        return true;
+    }
+
+    //class that handles the swipes events
+    private class SwipeDetecotr extends GestureDetector.SimpleOnGestureListener
+    {
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY)
+        {
+            //get x , y coordinates of where swipe started
+            float startX = event1.getX();
+            float startY = event1.getY();
+
+            //get x , y coordinates of where the swipe ended
+            float endX = event2.getX();
+            float endY = event2.getY();
+
+            System.out.println("X started at  : " +(int)startX);
+            System.out.println("X ended at : " +(int)endX);
+
+            System.out.println("Y started at  : " + (int)startY);
+            System.out.println("Y ended at : " +(int)endY);
+
+            return true;
+        }
     }
 }
